58b20094365fb76cc43c75474ac20f2238e5369c
Emmanuel RUKUNDO <e.rukundo@bk.rw>
2020-10-20 05:54:19 +0200

    initialize the project

---
+++ src/main/java/com/example/gruppe/GruppeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gruppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruppeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gruppe/GruppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gruppe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
